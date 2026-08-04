58a5517291a2cc8d2afeacbf73d11682412677f7
androidappr <viralandroidblog@gmail.com>
2026-08-04 09:56:47 +0545

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -281,7 +281,7 @@ def get_explicit_category(entry, link, source_name):
         ("Economic News", ["/economy/", "/economic/", "/arthik/", "economy", "economic", "arthik", "budget", "अर्थतन्त्र", "बजेट"]),
         ("Business News", ["/business/", "/wyapar/", "/bazar/", "business", "wyapar", "bazar", "bank", "व्यापार", "बैंक", "बजार"]),
         ("International News", ["/world/", "/international/", "/bidesh/", "/videsh/", "world", "international", "bidesh", "videsh", "विश्व", "अन्तर्राष्ट्रिय", "विदेश"]),
-        ("National News", ["/national/", "/pradesh/", "/desh/", "national", "pradesh", "desh", "राष्ट्रिय", "प्रदेश"])
+        ("National News", ["/national/", "/pradesh/", "/desh/","/national/", "national", "pradesh", "desh", "राष्ट्रिय", "प्रदेश","राष्ट्रिय समाचार"])
     ]
 
     for cat_name, patterns in mappings:
